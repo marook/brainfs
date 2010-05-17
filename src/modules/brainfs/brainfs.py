ae8d3ecf0a66f278f6b3f726fcd7bbcd52fd6ed6
Markus Pielmeier <markus.pielmeier@gmx.de>
2010-05-17 20:31:00 +0200

    addes some basic structure (adopted from tagfs http://github.com/marook/tagfs/)

---
+++ src/modules/brainfs/brainfs.py
@@ -0,0 +1,61 @@
+#
+# Copyright 2010 Markus Pielmeier
+#
+# This file is part of brainfs.
+#
+# brainfs is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# brainfs is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with brainfs.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import errno
+import fuse
+
+if not hasattr(fuse, '__version__'):
+    raise RuntimeError, \
+        "your fuse-py doesn't know of fuse.__version__, probably it's too old."
+
+fuse.fuse_python_api = (0, 2)
+
+class BrainFS(fuse.Fuse):
+
+    def __init__(self, initwd, *args, **kw):
+        fuse.Fuse.__init__(self, *args, **kw)
+
+    def getattr(self, path):
+        return -errno.ENOENT
+
+    def readdir(self, path, offset):
+        pass
+            
+    def readlink(self, path):
+        return -errno.ENOENT
+
+    def open(self, path, flags):
+        return -errno.ENOENT
+
+    def read(self, path, size, offset):
+        return -errno.ENOENT
+
+def main():
+    fs = BrainFS(os.getcwd(),
+            version = "%prog " + fuse.__version__,
+            dash_s_do = 'setsingle')
+
+    fs.parse(errex = 1)
+    opts, args = fs.cmdline
+
+    return fs.main()
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main())
